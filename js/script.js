f924059b171cbbb4064bc31caecdeb5d14ff0e6c
matt-carmo <matheus_2018@hotmail.com>
2022-03-02 15:16:50 -0300

    commit-2

--- js/script.js
+++ js/script.js
@@ -5,12 +5,18 @@ $('.carrosel').slick({
 : 2000,
 arrows: false
 });
+const prevent = document.querySelector('.prevent')
+
+
+
 
 const url = document.querySelector('.url')
 const buttonSend = document.querySelector('.button-send')
 const imgWeb = document.querySelector('.img-web')
 const name = document.querySelector('.name')
-buttonSend.addEventListener('click', () => {
+buttonSend.addEventListener('click', (e) => {
+
+      e.preventDefault()
       imgWeb.innerHTML = ` 
 
       <div class="img-web">
